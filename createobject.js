7facbcfdd79a4b8a7d5f4ee47e2d0d099d84881e
Toriqul Islam Tonu <50527978+toriqultonu@users.noreply.github.com>
2021-06-20 02:56:50 -0700

    creating object

--- createobject.js
+++ createobject.js
@@ -1,10 +1,16 @@
 const location = {
 	city: 'Rajshahi',
 	zipCode: '8363',
-	country: 'Bangladesh'
+	country: 'Bangladesh',
 	getFormattedAddress(){
 		return `${this.city} - ${this.zipCode} - ${this.country}`;
 	}
+	
 }
+const{city,...rest} = location;
+
+console.log(city);
+console.log(rest);
 
 console.log(location.getFormattedAddress());
+
